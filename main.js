69e1998c56f2acd22e771ad3fc72059f64ff7444
hasegawa2073 <hasegawatatsuya206@gmail.com>
2023-04-20 16:33:02 +0900

    ドーナツ型(TorusGeometry)を触った

--- main.js
+++ main.js
@@ -25,6 +25,7 @@ document.body.appendChild(renderer.domElement);
 const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
 const sphereGeometry = new THREE.SphereGeometry(0.5, 32, 16);
 const planeGeometry = new THREE.PlaneGeometry(5, 5);
+const torusGeometry = new THREE.TorusGeometry(8, 3, 10, 100);
 
 //マテリアル
 const material = new THREE.MeshNormalMaterial();
@@ -33,11 +34,12 @@ const material = new THREE.MeshNormalMaterial();
 const box = new THREE.Mesh(boxGeometry, material);
 const sphere = new THREE.Mesh(sphereGeometry, material);
 const plane = new THREE.Mesh(planeGeometry, material);
+const torus = new THREE.Mesh(torusGeometry, material);
 
 sphere.position.x = 1.5;
 plane.rotation.x = -Math.PI * 0.5;
 plane.position.y = -0.5;
-scene.add(box, sphere, plane);
+scene.add(box, sphere, plane, torus);
 
 //ライト
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.8);
